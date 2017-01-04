b63f0c3d1c804c6666a69e6fcb4b467f38e11608
yanxiaodi <929213769@qq.com>
2017-01-05 00:16:10 +0800

    完成首页顶部效果

--- src/components/Auto/DiscoverAppBar.vue
+++ src/components/Auto/DiscoverAppBar.vue
@@ -1,10 +1,10 @@
 <!-- "发现"页面 - 顶部AppBar -->
 <template>
-     <!--  title="Title"  -->
     <mu-appbar id="discoverAppBar" >
         <mu-icon-button icon='list' slot="left" />
-        <mu-text-field icon="search" class="appbar-search-field"  slot="right" hintText="S"/>
-        <mu-flat-button icon='chat' slot="right"/>
+        <mu-text-field icon="search" class="appbar-search-field"  slot="right" />
+        <mu-icon-button icon='chat' slot="right" />
+        <!--<mu-flat-button icon='' slot=""/>-->
     </mu-appbar>
 </template>
 
@@ -25,9 +25,24 @@
 // '发现' - 顶部AppBar栏
 #discoverAppBar
     position: fixed
-    +bC($F,.1)
-    filter: blur(1px)
+    +bC(rgba($F,.1))
     opacity: 1
-    .mu-ripple-wrapper                                                                      // 两侧按钮样式
-        color: $F
+    .mu-icon                                                                      // 两侧按钮样式
+        color: $F !important
+.mu-appbar-title
+    width: 0 !important
+.right
+    .mu-text-field
+        margin-bottom: 0 !important
+        >.mu-text-field-icon                                                    // 控制放大镜的位置定位
+            left: 37% !important
+            top: 25% !important
+        >div>div
+            .mu-text-field-line,.mu-text-field-focus-line
+                left: 0 !important
+    .mu-text-field.has-icon
+        padding-left: 37% !important
+    .focus-state
+        >i.mu-icon
+            display: none !important
 </style>
