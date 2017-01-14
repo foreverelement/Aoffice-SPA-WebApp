00054f5dc096c4c549e717b466f6c47a93f20fb0
yanxiaodi <929213769@qq.com>
2017-01-14 12:52:16 +0800

    test pages

--- src/components/Auto/Sidebar.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1> sidebar </h1>
-</template>
--- src/store/getters.js
+++
@@ -1,6 +0,0 @@
-// Getters 接收 store 的 state 作为第一个参数
-export const bannerImg_Arr              =   state   => state.bannerImg_Arr
-export const hotBuildingList_Arr        =   state   => state.hotBuildingList_Arr
-// 后期将modules 分出
-export const furnitureInfo              =   state   => state.furnitureInfo                   // 家具 详情页 信息内容
-export const researchInfo               =   state   => state.researchInfo                    // 外出 详情页 信息内容
