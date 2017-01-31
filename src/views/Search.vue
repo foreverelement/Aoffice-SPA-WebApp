15d14241b0481eb90f808a97190e01870c3e12e4
yanxiaodi <929213769@qq.com>
2017-01-31 23:43:56 +0800

    :lipstick:  优化搜索页 的样式

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -2,7 +2,7 @@
 <template>
     <div>
         <!-- 搜索AppBar -->
-        <div class="search__appBar">
+        <div id="search__appBar">
             <mu-text-field icon="search" class="appbar-search-field"  slot="right" hintText="直接输入大厦名称搜索"/>
             <mu-flat-button color="white" label="取消" slot="right" @click="goBack" />
         </div>
@@ -30,4 +30,22 @@ export default {
 <style lang="sass" scoped>
 @import '../sass/main.sass'
 
+#search__appBar
+    +bC($F)
+    // 输入框背景色
+    .mu-text-field
+        +REM(margin,$autoMargin/2)
+        margin:
+            left: 2%
+            right: 2%
+        width: 76% !important
+        +bC($base-color)
+    // 按钮背景色修改
+    .mu-flat-button
+        width: 19%
+        +bC($F)
+        color: $btnTitle-color !important
+        // "取消"按钮两侧的padding设为空
+        span.mu-flat-button-label
+            padding: 0 !important
 </style>
